1721aa0b9acc5133bfbc8791cbbef101365be440
bizz84 <bizz84@gmail.com>
2018-10-16 08:28:57 +0100

    Add Container, Column, SizedBox to SignInPage

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -8,6 +8,20 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.orange,
+              child: SizedBox(
+                width: 100.0,
+                height: 100.0,
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
