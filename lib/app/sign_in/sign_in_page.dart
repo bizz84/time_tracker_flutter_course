7b995ed00bb2b49932ac28bd47f6ff8ff55d06b6
bizz84 <bizz84@gmail.com>
2018-10-19 11:54:28 +0100

    Add more boxes and extract to method

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -8,19 +8,35 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100.0,
-              ),
+      body: buildContent(),
+    );
+  }
+
+  Widget buildContent() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100.0,
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+        ],
       ),
     );
   }
