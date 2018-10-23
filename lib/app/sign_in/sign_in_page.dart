aa5a058679ac2d9adf2f4de86128ae1fc557d995
bizz84 <bizz84@gmail.com>
2018-10-23 20:55:50 +0100

    Replace CustomRaisedButton with SignInButton in SignInPage

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
 
 class SignInPage extends StatelessWidget {
@@ -30,16 +31,10 @@ class SignInPage extends StatelessWidget {
             ),
           ),
           SizedBox(height: 8.0),
-          CustomRaisedButton(
-            child: Text(
-              'Sign in with Google',
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 15.0,
-              ),
-            ),
+          SignInButton(
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
             color: Colors.white,
-            borderRadius: 4.0,
             onPressed: () {},
           ),
         ],
