50d6830451bc5305f448c1d77df4d96c99ac3220
bizz84 <bizz84@gmail.com>
2018-10-23 18:55:05 +0100

    Add CustomRaisedButton

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -29,7 +30,7 @@ class SignInPage extends StatelessWidget {
             ),
           ),
           SizedBox(height: 8.0),
-          RaisedButton(
+          CustomRaisedButton(
             child: Text(
               'Sign in with Google',
               style: TextStyle(
@@ -38,11 +39,7 @@ class SignInPage extends StatelessWidget {
               ),
             ),
             color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(4.0),
-              ),
-            ),
+            borderRadius: 4.0,
             onPressed: () {},
           ),
         ],
---
+++ lib/common_widgets/custom_raised_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  CustomRaisedButton({
+    this.child,
+    this.color,
+    this.borderRadius,
+    this.onPressed,
+  });
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: child,
+      color: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(borderRadius),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
