a972f6f30b5bf95761378d2da21be30eaeb6de85
bizz84 <bizz84@gmail.com>
2018-10-23 20:20:59 +0100

    Add SignInButton class

---
+++ lib/app/sign_in/sign_in_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+
+class SignInButton extends CustomRaisedButton {
+  SignInButton({
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Text(
+            text,
+            style: TextStyle(color: textColor, fontSize: 15.0),
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}
