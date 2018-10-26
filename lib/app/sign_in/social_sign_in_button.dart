fbdbe9e7f2bd968274e429e78d34dda2cf65daac
bizz84 <bizz84@gmail.com>
2018-10-26 16:23:10 +0100

    Add SocialSignInButton

---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    String assetName,
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset(assetName),
+              Text(
+                text,
+                style: TextStyle(color: textColor, fontSize: 15.0),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Image.asset(assetName),
+              ),
+            ],
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}
