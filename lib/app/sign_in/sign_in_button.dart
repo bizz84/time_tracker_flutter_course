af3232c754cfc963b6b1da0ee3a4045f6b4cec2c
bizz84 <bizz84@gmail.com>
2018-10-29 13:07:33 +0000

    Add assertions to custom buttons

--- lib/app/sign_in/sign_in_button.dart
+++ lib/app/sign_in/sign_in_button.dart
@@ -7,7 +7,8 @@ class SignInButton extends CustomRaisedButton {
     Color color,
     Color textColor,
     VoidCallback onPressed,
-  }) : super(
+  }) : assert(text != null),
+        super(
           child: Text(
             text,
             style: TextStyle(color: textColor, fontSize: 15.0),
--- lib/common_widgets/custom_raised_button.dart
+++ lib/common_widgets/custom_raised_button.dart
@@ -7,7 +7,7 @@ class CustomRaisedButton extends StatelessWidget {
     this.borderRadius: 2.0,
     this.height: 50.0,
     this.onPressed,
-  });
+  }) : assert(borderRadius != null);
   final Widget child;
   final Color color;
   final double borderRadius;
