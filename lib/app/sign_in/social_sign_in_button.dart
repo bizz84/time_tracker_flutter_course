d6be3670226c2594c29a5ab357982762095c2f5c
bizz84 <bizz84@gmail.com>
2018-10-29 13:07:33 +0000

    Add assertions to custom buttons

--- lib/app/sign_in/social_sign_in_button.dart
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -8,7 +8,9 @@ class SocialSignInButton extends CustomRaisedButton {
     Color color,
     Color textColor,
     VoidCallback onPressed,
-  }) : super(
+  })  : assert(assetName != null),
+        assert(text != null),
+        super(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
