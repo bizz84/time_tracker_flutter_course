b87062417e938648126f1720ebcce7fa443db2e2
bizz84 <bizz84@gmail.com>
2018-12-03 13:03:03 +0000

    Toggling the form type

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,17 +1,41 @@
-
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
 
+class EmailSignInForm extends StatefulWidget {
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
   void _submit() {
-    print('email: ${_emailController.text}, password: ${_passwordController.text}');
+    print(
+        'email: ${_emailController.text}, password: ${_passwordController.text}');
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn ?
+          EmailSignInFormType.register : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
   }
 
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+
     return [
       TextField(
         controller: _emailController,
@@ -30,13 +54,13 @@ class EmailSignInForm extends StatelessWidget {
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        text: 'Sign in',
+        text: primaryText,
         onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       FlatButton(
-        child: Text('Need an account? Register'),
-        onPressed: () {},
+        child: Text(secondaryText),
+        onPressed: _toggleFormType,
       ),
     ];
   }
