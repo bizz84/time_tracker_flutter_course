8a443a20eefe1a6079ab5300409f9c73c4698a27
bizz84 <bizz84@gmail.com>
2018-12-03 10:11:05 +0000

    Add email and password fields to EmailSignInForm

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -2,8 +2,32 @@
 import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
+
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'test@test.com',
+        ),
+      ),
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
   }
 }
