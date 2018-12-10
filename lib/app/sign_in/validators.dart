1f7bec168288b7b3945234dac7cc69dae5ee1046
bizz84 <bizz84@gmail.com>
2018-12-10 13:30:09 +0000

    Add NonEmptyStringValidator

---
+++ lib/app/sign_in/validators.dart
@@ -0,0 +1,11 @@
+
+abstract class StringValidator {
+  bool isValid(String value);
+}
+
+class NonEmptyStringValidator implements StringValidator {
+  @override
+  bool isValid(String value) {
+    return value.isNotEmpty;
+  }
+}
