a25300d10581a3ae4bc4cf00850241a7b0410b64
bizz84 <bizz84@gmail.com>
2019-02-04 14:32:11 +0000

    Create PlatformAlertDialog class

---
+++ lib/common_widgets/platform_alert_dialog.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_widget.dart';
+
+class PlatformAlertDialog extends PlatformWidget {
+  PlatformAlertDialog({
+    @required this.title,
+    @required this.content,
+    @required this.defaultActionText,
+  })  : assert(title != null),
+        assert(content != null),
+        assert(defaultActionText != null);
+
+  final String title;
+  final String content;
+  final String defaultActionText;
+
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: _buildActions(context),
+    );
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: _buildActions(context),
+    );
+  }
+
+  List<Widget> _buildActions(BuildContext context) {
+    return [
+      PlatformAlertDialogAction(
+        child: Text(defaultActionText),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+    ];
+  }
+}
+
+class PlatformAlertDialogAction extends PlatformWidget {
+  PlatformAlertDialogAction({this.child, this.onPressed});
+  final Widget child;
+  final VoidCallback onPressed;
+
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return CupertinoDialogAction(
+      child: child,
+      onPressed: onPressed,
+    );
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return FlatButton(
+      child: child,
+      onPressed: onPressed,
+    );
+  }
+
+}
