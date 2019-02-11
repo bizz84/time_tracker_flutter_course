5b75b3b865ea355a0f8b354c997e4a9e4286dfbf
bizz84 <bizz84@gmail.com>
2019-02-11 10:41:01 +0000

    Creating the AuthProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/auth_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: LandingPage(
-        auth: Auth(),
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        title: 'Time Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: LandingPage(
+          auth: Auth(),
+        ),
       ),
     );
   }
---
+++ lib/services/auth_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class AuthProvider extends InheritedWidget {
+  AuthProvider({@required this.auth, @required this.child});
+  final AuthBase auth;
+  final Widget child;
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => false;
+
+  // final auth = AuthProvider.of(context);
+  static AuthBase of(BuildContext context) {
+    AuthProvider provider = context.inheritFromWidgetOfExactType(AuthProvider);
+    return provider.auth;
+  }
+}
