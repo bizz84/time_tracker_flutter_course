1d4dfc49b08b0b3a0933363fbc6c6221350d0fce
bizz84 <bizz84@gmail.com>
2019-05-10 15:18:50 +0100

    Handle firestore permissions errors

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 import 'package:time_tracker_flutter_course/common_widgets/platform_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 import 'package:time_tracker_flutter_course/services/database.dart';
+import 'package:flutter/services.dart';
 
 class JobsPage extends StatelessWidget {
   Future<void> _signOut(BuildContext context) async {
@@ -28,8 +30,16 @@ class JobsPage extends StatelessWidget {
   }
 
   Future<void> _createJob(BuildContext context) async {
-    final database = Provider.of<Database>(context);
-    await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+
+    try {
+      final database = Provider.of<Database>(context);
+      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(
+        title: 'Operation failed',
+        exception: e,
+      ).show(context);
+    }
   }
 
 
--- lib/common_widgets/platform_exception_alert_dialog.dart
+++ lib/common_widgets/platform_exception_alert_dialog.dart
@@ -13,6 +13,11 @@ class PlatformExceptionAlertDialog extends PlatformAlertDialog {
   );
 
   static String _message(PlatformException exception) {
+    if (exception.message == 'FIRFirestoreErrorDomain') {
+      if (exception.code == 'Error 7') {
+        return 'Missing or insufficient permissions';
+      }
+    }
     return _errors[exception.code] ?? exception.message;
   }
 
