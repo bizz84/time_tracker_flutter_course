0b58f7d11593265505d8ce06c38851177e2c2551
bizz84 <bizz84@gmail.com>
2019-07-03 16:20:16 +0100

    Validating and saving the form

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -15,12 +15,42 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name;
+  int _ratePerHour;
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('form saved, name: $_name, ratePerHour: $_ratePerHour');
+      // TODO: submit data to Firestore
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text('New Job'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Save',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            onPressed: _submit,
+          ),
+        ],
       ),
       body: _buildContents(),
       backgroundColor: Colors.grey[200],
@@ -43,6 +73,7 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildForm() {
     return Form(
+      key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: _buildFormChildren(),
@@ -54,6 +85,8 @@ class _AddJobPageState extends State<AddJobPage> {
     return [
       TextFormField(
         decoration: InputDecoration(labelText: 'Job name'),
+        validator: (value) => value.isNotEmpty ? null : 'Name can\'t be empty',
+        onSaved: (value) => _name = value,
       ),
       TextFormField(
         decoration: InputDecoration(labelText: 'Rate per hour'),
@@ -61,6 +94,7 @@ class _AddJobPageState extends State<AddJobPage> {
           signed: false,
           decimal: false,
         ),
+        onSaved: (value) => _ratePerHour = int.parse(value) ?? 0,
       ),
     ];
   }
