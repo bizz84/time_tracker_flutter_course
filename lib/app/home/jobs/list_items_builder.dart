68e34675fbaf424891c156597c87916e3d8f25b2
bizz84 <bizz84@gmail.com>
2019-07-11 11:03:51 +0100

    Add ListItemsBuilder

---
+++ lib/app/home/jobs/list_items_builder.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/empty_content.dart';
+
+typedef ItemWidgetBuilder<T> = Widget Function(BuildContext context, T item);
+
+class ListItemsBuilder<T> extends StatelessWidget {
+  const ListItemsBuilder({Key key, this.snapshot, this.itemBuilder})
+      : super(key: key);
+  final AsyncSnapshot<List<T>> snapshot;
+  final ItemWidgetBuilder<T> itemBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    if (snapshot.hasData) {
+      final List<T> items = snapshot.data;
+      if (items.isNotEmpty) {
+        return _buildList(items);
+      } else {
+        return EmptyContent();
+      }
+    } else if (snapshot.hasError) {
+      return EmptyContent(
+        title: 'Something went wrong',
+        message: 'Can\'t load items right now',
+      );
+    }
+    return Center(child: CircularProgressIndicator());
+  }
+
+  Widget _buildList(List<T> items) {
+
+  }
+}
