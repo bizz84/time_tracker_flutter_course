7adbc7a3a04f3c445c447d02c49e7ede16ce7826
bizz84 <bizz84@gmail.com>
2019-07-29 14:59:14 +0100

    Adding the CupertinoTabScaffold

--- lib/app/home/cupertino_home_scaffold.dart
+++ lib/app/home/cupertino_home_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
@@ -13,6 +14,30 @@ class CupertinoHomeScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          _buildItem(TabItem.jobs),
+          _buildItem(TabItem.entries),
+          _buildItem(TabItem.account),
+        ],
+        onTap: (index) => onSelectTab(TabItem.values[index]),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem tabItem) {
+    final itemData = TabItemData.allTabs[tabItem];
+    final color = currentTab == tabItem ? Colors.indigo : Colors.grey;
+    return BottomNavigationBarItem(
+      icon: Icon(
+        itemData.icon,
+        color: color,
+      ),
+      title: Text(
+        itemData.title,
+        style: TextStyle(color: color),
+      ),
+    );
   }
 }
