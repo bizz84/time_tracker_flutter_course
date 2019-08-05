b140c9e14f2bdc1728944d1401b392728a72e86a
bizz84 <bizz84@gmail.com>
2019-08-05 12:22:13 +0100

    Adding the new entries files

---
+++ lib/app/home/entries/entries_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_bloc.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_list_tile.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/list_items_builder.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
+
+class EntriesPage extends StatelessWidget {
+  static Widget create(BuildContext context) {
+    final database = Provider.of<Database>(context);
+    return Provider<EntriesBloc>(
+      builder: (_) => EntriesBloc(database: database),
+      child: EntriesPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Entries'),
+        elevation: 2.0,
+      ),
+      body: _buildContents(context),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final bloc = Provider.of<EntriesBloc>(context);
+    return StreamBuilder<List<EntriesListTileModel>>(
+      stream: bloc.entriesTileModelStream,
+      builder: (context, snapshot) {
+        return ListItemsBuilder<EntriesListTileModel>(
+          snapshot: snapshot,
+          itemBuilder: (context, model) => EntriesListTile(model: model),
+        );
+      },
+    );
+  }
+}
--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/account/account_page.dart';
 import 'package:time_tracker_flutter_course/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_page.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
@@ -22,7 +23,7 @@ class _HomePageState extends State<HomePage> {
   Map<TabItem, WidgetBuilder> get widgetBuilders {
     return {
       TabItem.jobs: (_) => JobsPage(),
-      TabItem.entries: (_) => Container(),
+      TabItem.entries: (context) => EntriesPage.create(context),
       TabItem.account: (_) => AccountPage(),
     };
   }
