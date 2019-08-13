22f0916fd5cf6043719954e14e06ee5d7716f7cb
bizz84 <bizz84@gmail.com>
2019-08-13 16:11:43 +0100

    Add date tests with GB locale

--- test/format_test.dart
+++ test/format_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:time_tracker_flutter_course/app/home/job_entries/format.dart';
 
 void main() {
-
   group('hours', () {
     test('positive', () {
       expect(Format.hours(10), '10h');
@@ -18,6 +19,25 @@ void main() {
 
     test('decimal', () {
       expect(Format.hours(4.5), '4.5h');
+    });
+  });
+
+  group('date - GB Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_GB';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('2019-08-12', () {
+      expect(
+        Format.date(DateTime(2019, 8, 12)),
+        '12 Aug 2019',
+      );
+    });
+    test('2019-08-16', () {
+      expect(
+        Format.date(DateTime(2019, 8, 16)),
+        '16 Aug 2019',
+      );
     });
   });
 }
