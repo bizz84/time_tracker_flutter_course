87997a15c0c05d7dd9a61c3c62fc19f5449afd67
bizz84 <bizz84@gmail.com>
2019-08-13 09:48:13 +0100

    Defensive check on entries page

--- lib/app/home/entries/entries_bloc.dart
+++ lib/app/home/entries/entries_bloc.dart
@@ -32,6 +32,9 @@ class EntriesBloc {
       _allEntriesStream.map(_createModels);
 
   static List<EntriesListTileModel> _createModels(List<EntryJob> allEntries) {
+    if (allEntries.isEmpty) {
+      return [];
+    }
     final allDailyJobsDetails = DailyJobsDetails.all(allEntries);
 
     // total duration across all jobs
