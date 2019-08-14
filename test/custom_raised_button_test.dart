93e58e2fe3e6fe6f647931385417695c96c9b76a
bizz84 <bizz84@gmail.com>
2019-08-14 16:52:00 +0100

    Testing widget callback

--- test/custom_raised_button_test.dart
+++ test/custom_raised_button_test.dart
@@ -3,15 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
 
 void main() {
-  testWidgets('', (WidgetTester tester) async {
+  testWidgets('onPressed callback', (WidgetTester tester) async {
+    var pressed = false;
     await tester.pumpWidget(
       MaterialApp(home: CustomRaisedButton(
         child: Text('tap me'),
+        onPressed: () => pressed = true,
       )),
     );
     final button = find.byType(RaisedButton);
     expect(button, findsOneWidget);
     expect(find.byType(FlatButton), findsNothing);
     expect(find.text('tap me'), findsOneWidget);
+    await tester.tap(button);
+    expect(pressed, true);
   });
 }
