e66abac5f9df790fd4e48eac7bb047e9b3e5c82d
bizz84 <bizz84@gmail.com>
2019-08-14 09:25:46 +0100

    Completing the Format tests

--- test/format_test.dart
+++ test/format_test.dart
@@ -40,4 +40,49 @@ void main() {
       );
     });
   });
+
+  group('dayOfWeek - GB locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_GB';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Monday', () {
+      expect(
+        Format.dayOfWeek(DateTime(2019, 8, 12)),
+        'Mon',
+      );
+    });
+
+  });
+  group('dayOfWeek - IT locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'it_IT';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Lunedì', () {
+      expect(
+        Format.dayOfWeek(DateTime(2019, 8, 12)),
+        'lun',
+      );
+    });
+
+  });
+
+  group('currency - US locale', () {
+    setUp(() {
+      Intl.defaultLocale = 'en_US';
+    });
+    test('positive', () {
+      expect(Format.currency(10), '\$10');
+    });
+
+    test('zero', () {
+      expect(Format.currency(0), '');
+    });
+
+    test('negative', () {
+      expect(Format.currency(-5), '-\$5');
+    });
+
+  });
 }
