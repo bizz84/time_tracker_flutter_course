dddd9b383f500abf497732472768e15f422501bf
bizz84 <bizz84@gmail.com>
2019-08-16 15:04:18 +0100

    Add first registration test

--- test/email_sign_in_form_stateful_test.dart
+++ test/email_sign_in_form_stateful_test.dart
@@ -65,4 +65,46 @@ void main() {
       verify(mockAuth.signInWithEmailAndPassword(email, password)).called(1);
     });
   });
+
+  group('register', () {
+    testWidgets(
+        'WHEN user taps on the secondary button'
+        'THEN form toggles to registration mode', (WidgetTester tester) async {
+      await pumpEmailSignInForm(tester);
+
+      final registerButton = find.text('Need an account? Register');
+      await tester.tap(registerButton);
+
+      await tester.pump();
+
+      final createAccountButton = find.text('Create an account');
+      expect(createAccountButton, findsOneWidget);
+    });
+
+    testWidgets(
+        'WHEN user enters the email and password'
+        'AND user taps on the sign-in button'
+        'THEN signInWithEmailAndPassword is called',
+        (WidgetTester tester) async {
+      await pumpEmailSignInForm(tester);
+
+      const email = 'email@email.com';
+      const password = 'password';
+
+      final emailField = find.byKey(Key('email'));
+      expect(emailField, findsOneWidget);
+      await tester.enterText(emailField, email);
+
+      final passwordField = find.byKey(Key('password'));
+      expect(passwordField, findsOneWidget);
+      await tester.enterText(passwordField, password);
+
+      await tester.pump();
+
+      final signInButton = find.text('Sign in');
+      await tester.tap(signInButton);
+
+      verify(mockAuth.signInWithEmailAndPassword(email, password)).called(1);
+    });
+  });
 }
