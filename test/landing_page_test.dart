a2d11833af88fabcf4a89b5b72a972f5bbc2ad9f
bizz84 <bizz84@gmail.com>
2019-08-19 15:33:57 +0100

    Complete the landing page tests

--- test/landing_page_test.dart
+++ test/landing_page_test.dart
@@ -1,17 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/home_page.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class MockAuth extends Mock implements AuthBase {}
 
 void main() {
   MockAuth mockAuth;
+  StreamController<User> onAuthStateChangedController;
 
   setUp(() {
     mockAuth = MockAuth();
+    onAuthStateChangedController = StreamController<User>();
+  });
+
+  tearDown(() {
+    onAuthStateChangedController.close();
   });
 
   Future<void> pumpLandingPage(WidgetTester tester) async {
@@ -23,11 +33,38 @@ void main() {
         ),
       ),
     );
+    await tester.pump();
   }
 
   void stubOnAuthStateChangedYields(Iterable<User> onAuthStateChanged) {
+    onAuthStateChangedController
+        .addStream(Stream<User>.fromIterable(onAuthStateChanged));
     when(mockAuth.onAuthStateChanged).thenAnswer((_) {
-      return Stream<User>.fromIterable(onAuthStateChanged);
+      return onAuthStateChangedController.stream;
     });
   }
+
+  testWidgets('stream waiting', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('null user', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([null]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(SignInPage), findsOneWidget);
+  });
+
+  testWidgets('non-null user', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([User(uid: '123')]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(HomePage), findsOneWidget);
+  });
 }
