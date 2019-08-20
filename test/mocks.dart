9840643bd4fe63c428c5839f055f3dd20a25203c
bizz84 <bizz84@gmail.com>
2019-08-20 10:15:27 +0100

    Testing navigation

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,5 +1,8 @@
 
+import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class MockAuth extends Mock implements AuthBase {}
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
