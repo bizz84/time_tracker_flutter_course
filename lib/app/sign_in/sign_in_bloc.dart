d40330b2a31fd40c11b5d266f38a82b45d15b574
bizz84 <bizz84@gmail.com>
2019-09-16 10:16:56 +0100

    Add authentication code to the sign in bloc

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -1,6 +1,13 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
 class SignInBloc {
+  SignInBloc({@required this.auth});
+  final AuthBase auth;
+
   final StreamController<bool> _isLoadingController = StreamController<bool>();
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
@@ -8,5 +15,22 @@ class SignInBloc {
     _isLoadingController.close();
   }
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
+  }
+
+  Future<User> signInAnonymously() async => await _signIn(auth.signInAnonymously);
+
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+
+  Future<User> signInWithFacebook() async => await _signIn(auth.signInWithFacebook);
 }
