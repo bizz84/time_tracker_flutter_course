7a6c0e359a923e3f214c75e8ba1844e088347311
bizz84 <bizz84@gmail.com>
2019-10-17 12:53:49 +0100

    Add onAuthStateChanged stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -22,6 +22,10 @@ class Auth implements AuthBase {
     return User(uid: user.uid);
   }
 
+  Stream<User> get onAuthStateChanged {
+    return _firebaseAuth.onAuthStateChanged.map(_userFromFirebase);
+  }
+
   @override
   Future<User> currentUser() async {
     final user = await _firebaseAuth.currentUser();
