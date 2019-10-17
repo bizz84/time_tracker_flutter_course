09a453db2be5bf7b622f7bb24b4ef29ef5dfbe4a
bizz84 <bizz84@gmail.com>
2019-10-17 10:12:12 +0100

    Add User class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class User {
+  User({@required this.uid});
+  final String uid;
+}
 
 class Auth {
   final _firebaseAuth = FirebaseAuth.instance;
 
-  Future<FirebaseUser> currentUser() async {
-    return await _firebaseAuth.currentUser();
+  User _userFromFirebase(FirebaseUser user) {
+    if (user == null) {
+      return null;
+    }
+    return User(uid: user.uid);
   }
-  Future<FirebaseUser> signInAnonymously() async {
+
+  Future<User> currentUser() async {
+    final user = await _firebaseAuth.currentUser();
+    return _userFromFirebase(user);
+  }
+  Future<User> signInAnonymously() async {
     final authResult = await _firebaseAuth.signInAnonymously();
-    return authResult.user;
+    return _userFromFirebase(authResult.user);
   }
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
