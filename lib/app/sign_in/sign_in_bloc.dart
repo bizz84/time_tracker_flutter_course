c5f1c910d39c7e9add29419102b6e29bbf743969
bizz84 <bizz84@gmail.com>
2019-10-23 15:04:39 +0100

    Refactor SignInBloc to use ValueNotifier

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -4,24 +4,16 @@ import 'package:flutter/foundation.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInBloc {
-  SignInBloc({@required this.auth});
+  SignInBloc({@required this.auth, @required this.isLoading});
   final AuthBase auth;
-
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
-
-  void dispose() {
-    _isLoadingController.close();
-  }
-
-  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  final ValueNotifier<bool> isLoading;
 
   Future<User> _signIn(Future<User> Function() signInMethod) async {
     try {
-      _setIsLoading(true);
+      isLoading.value = true;
       return await signInMethod();
     } catch (e) {
-      _setIsLoading(false);
+      isLoading.value = false;
       rethrow;
     }
   }
