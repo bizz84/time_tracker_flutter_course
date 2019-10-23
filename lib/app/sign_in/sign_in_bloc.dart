759972d9b092d22a7f15f72b40a4aa8fca367f24
bizz84 <bizz84@gmail.com>
2019-10-23 13:21:07 +0100

    Fix SignInBloc submit method

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -22,9 +22,8 @@ class SignInBloc {
       _setIsLoading(true);
       return await signInMethod();
     } catch (e) {
-      rethrow;
-    } finally {
       _setIsLoading(false);
+      rethrow;
     }
   }
 
