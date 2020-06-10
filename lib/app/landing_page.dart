1e67593f81ba3433c037184f6b0791f6e575f342
Andrea Bizzotto <bizz84@gmail.com>
2020-06-10 21:36:29 +0100

    Fix warnings

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,9 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/home_page.dart';
-import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 import 'package:time_tracker_flutter_course/services/database.dart';
