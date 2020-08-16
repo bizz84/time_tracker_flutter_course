e641fdee23e0f5882b06c338d67708b513071a65
Chris Klanac <cklanac@users.noreply.github.com>
2020-08-16 12:19:45 -0400

    fix: error in debug console on anonymous logout Error: "Tried to listen to a value exposed with provider, from outside of the widget tree."

--- lib/app/home/account/account_page.dart
+++ lib/app/home/account/account_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/common_widgets/avatar.dart';
@@ -6,10 +5,9 @@ import 'package:time_tracker_flutter_course/common_widgets/platform_alert_dialog
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class AccountPage extends StatelessWidget {
-
   Future<void> _signOut(BuildContext context) async {
     try {
-      final auth = Provider.of<AuthBase>(context);
+      final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signOut();
     } catch (e) {
       print(e.toString());
