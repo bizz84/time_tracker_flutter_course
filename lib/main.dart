ad43d1b4f6a8b6cc513a090a095fc52cc295e200
Andrea Bizzotto <bizz84@gmail.com>
2020-11-06 09:30:21 +0000

    main() returns Future<void>

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
