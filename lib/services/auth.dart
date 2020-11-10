ef54838c40757329f1692a1da9cec108140b0393
Andrea Bizzotto <bizz84@gmail.com>
2020-11-10 14:30:11 +0000

    Add Google Sign Out

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -54,6 +54,8 @@ class Auth implements AuthBase {
 
   @override
   Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
     await _firebaseAuth.signOut();
   }
 }
