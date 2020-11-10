8df12385375349ade50efc968d3a4882e96dd108
Andrea Bizzotto <bizz84@gmail.com>
2020-11-10 09:47:02 +0000

    Add the authStateChanges() method

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthBase {
   User get currentUser;
+  Stream<User> authStateChanges();
   Future<User> signInAnonymously();
   Future<void> signOut();
 }
@@ -9,6 +10,9 @@ abstract class AuthBase {
 class Auth implements AuthBase {
 
   final _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Stream<User> authStateChanges() => _firebaseAuth.authStateChanges();
 
   @override
   User get currentUser => _firebaseAuth.currentUser;
