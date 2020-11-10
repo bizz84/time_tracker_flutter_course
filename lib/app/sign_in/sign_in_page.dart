bf06765d81dd62e0d0654e76d3eddf71b7eb1a24
Andrea Bizzotto <bizz84@gmail.com>
2020-11-10 13:43:31 +0000

    Hook up Google sign in in SignInPage

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -14,6 +14,15 @@ class SignInPage extends StatelessWidget {
       print(e.toString());
     }
   }
+
+  Future<void> _signInWithGoogle() async {
+    try {
+      await auth.signInWithGoogle();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +56,7 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with Google',
             textColor: Colors.black87,
             color: Colors.white,
-            onPressed: () {},
+            onPressed: _signInWithGoogle,
           ),
           SizedBox(height: 8.0),
           SocialSignInButton(
