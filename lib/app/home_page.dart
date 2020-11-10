c9d0841c8e028f8e7eaa262ca227361e55ec2448
Andrea Bizzotto <bizz84@gmail.com>
2020-11-10 11:38:27 +0000

    Refactor the sign-in-flows

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,16 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key, @required this.auth, @required this.onSignOut}) : super(key: key);
+  const HomePage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
-  final VoidCallback onSignOut;
 
   Future<void> _signOut() async {
     try {
       await auth.signOut();
-      onSignOut();
     } catch (e) {
       print(e.toString());
     }
--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -4,46 +4,24 @@ import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   const LandingPage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
 
   @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-
-  User _user;
-
-  @override
-  void initState() {
-    super.initState();
-    _updateUser(widget.auth.currentUser);
-  }
-
-  void _updateUser(User user) {
-    setState(() {
-      _user = user;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return StreamBuilder<User>(
-      stream: widget.auth.authStateChanges(),
+      stream: auth.authStateChanges(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.active) {
           final User user = snapshot.data;
           if (user == null) {
             return SignInPage(
-              auth: widget.auth,
-              onSignIn: _updateUser,
+              auth: auth,
             );
           }
           return HomePage(
-            auth: widget.auth,
-            onSignOut: () => _updateUser(null),
+            auth: auth,
           );
         }
         return Scaffold(
