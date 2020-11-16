9977fc1c5f9a8c1246dc286c6e90233968af5f1e
Andrea Bizzotto <bizz84@gmail.com>
2020-11-16 10:22:30 +0000

    Add bloc submit method

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -1,9 +1,14 @@
 
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_model.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class EmailSignInBloc {
+  EmailSignInBloc({@required this.auth});
+  final AuthBase auth;
+
   final StreamController<EmailSignInModel> _modelController = StreamController<EmailSignInModel>();
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
   EmailSignInModel _model = EmailSignInModel();
@@ -12,6 +17,19 @@ class EmailSignInBloc {
     _modelController.close();
   }
 
+  Future<void> submit() async {
+    updateWith(submitted: true, isLoading: true);
+    try {
+      if (_model.formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(_model.email, _model.password);
+      } else {
+        await auth.createUserWithEmailAndPassword(_model.email, _model.password);
+      }
+    } catch (e) {
+      updateWith(isLoading: false);
+      rethrow;
+    }
+  }
   void updateWith({
     String email,
     String password,
