0302a1d3ec3a1ff7ffd5427bd95702e5cd52433c
Andrea Bizzotto <bizz84@gmail.com>
2020-11-24 13:50:07 +0000

    Add _setData method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -12,9 +12,14 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
-  Future<void> createJob(Job job) async {
-    final path = APIPath.job(uid, 'job_abc');
-    final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(job.toMap());
+  Future<void> createJob(Job job) => _setData(
+    path: APIPath.job(uid, 'job_abc'),
+    data: job.toMap(),
+  );
+
+  Future<void> _setData({String path, Map<String, dynamic> data}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('$path: $data');
+    await reference.set(data);
   }
 }
