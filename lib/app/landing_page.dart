388529a60367f8ee42ddd84bf8ff6853bbc9c80f
Andrea Bizzotto <bizz84@gmail.com>
2020-11-24 10:25:50 +0000

    Rename HomePage to JobsPage

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:time_tracker_flutter_course/app/home_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
@@ -18,7 +18,7 @@ class LandingPage extends StatelessWidget {
           if (user == null) {
             return SignInPage.create(context);
           }
-          return HomePage();
+          return JobsPage();
         }
         return Scaffold(
           body: Center(
