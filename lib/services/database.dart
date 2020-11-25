4d2ebc33c97954fcbbc6f82d2dac30e1f83f89c1
Andrea Bizzotto <bizz84@gmail.com>
2020-11-25 12:00:07 +0000

    Add _collectionStream helper method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -17,18 +17,27 @@ class FirestoreDatabase implements Database {
         data: job.toMap(),
       );
 
-  Stream<List<Job>> jobsStream() {
-    final path = APIPath.jobs(uid);
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs.map(
-          (snapshot) => Job.fromMap(snapshot.data()),
-        ).toList());
-  }
+  Stream<List<Job>> jobsStream() => _collectionStream(
+        path: APIPath.jobs(uid),
+        builder: (data) => Job.fromMap(data),
+      );
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('$path: $data');
     await reference.set(data);
+  }
+
+  Stream<List<T>> _collectionStream<T>({
+    @required String path,
+    @required T Function(Map<String, dynamic> data) builder,
+  }) {
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => snapshot.docs
+        .map(
+          (snapshot) => builder(snapshot.data()),
+        )
+        .toList());
   }
 }
