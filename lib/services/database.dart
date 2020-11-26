741f15e4e85b4e7b8eaed0dcd4260f3da77ece74
Andrea Bizzotto <bizz84@gmail.com>
2020-11-26 12:01:18 +0000

    Add code to delete jobs in Database

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:time_tracker_flutter_course/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> setJob(Job job);
+  Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
 }
 
@@ -16,11 +17,18 @@ class FirestoreDatabase implements Database {
 
   final _service = FirestoreService.instance;
 
+  @override
   Future<void> setJob(Job job) => _service.setData(
         path: APIPath.job(uid, job.id),
         data: job.toMap(),
       );
 
+  @override
+  Future<void> deleteJob(Job job) => _service.deleteData(
+        path: APIPath.job(uid, job.id),
+      );
+
+  @override
   Stream<List<Job>> jobsStream() => _service.collectionStream(
         path: APIPath.jobs(uid),
         builder: (data, documentId) => Job.fromMap(data, documentId),
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -5,10 +5,19 @@ class FirestoreService {
   FirestoreService._();
   static final instance = FirestoreService._();
 
-  Future<void> setData({String path, Map<String, dynamic> data}) async {
+  Future<void> setData({
+    @required String path,
+    @required Map<String, dynamic> data,
+  }) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('$path: $data');
     await reference.set(data);
+  }
+
+  Future<void> deleteData({@required String path}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('delete: $path');
+    await reference.delete();
   }
 
   Stream<List<T>> collectionStream<T>({
