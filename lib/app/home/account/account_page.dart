26487359ee4927c78cc1bf46d401a3b64b1753bb
Andrea Bizzotto <bizz84@gmail.com>
2020-11-30 14:08:35 +0000

    Tweaking the account page

--- lib/app/home/account/account_page.dart
+++ lib/app/home/account/account_page.dart
@@ -54,9 +54,20 @@ class AccountPage extends StatelessWidget {
   }
 
   Widget _buildUserInfo(User user) {
-    return Avatar(
-      photoUrl: user.photoURL,
-      radius: 50,
+    return Column(
+      children: <Widget>[
+        Avatar(
+          photoUrl: user.photoURL,
+          radius: 50,
+        ),
+        SizedBox(height: 8),
+        if (user.displayName != null)
+          Text(
+            user.displayName,
+            style: TextStyle(color: Colors.white),
+          ),
+        SizedBox(height: 8),
+      ],
     );
   }
 }
