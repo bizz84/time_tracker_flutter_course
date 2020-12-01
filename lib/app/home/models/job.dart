edd8ff84a625a1fa666cf078a2a54c6c50df58db
Andrea Bizzotto <bizz84@gmail.com>
2020-12-01 09:28:42 +0000

    Adding hashCode and == operator

--- lib/app/home/models/job.dart
+++ lib/app/home/models/job.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:meta/meta.dart';
 
 class Job {
@@ -12,11 +13,7 @@ class Job {
     }
     final String name = data['name'];
     final int ratePerHour = data['ratePerHour'];
-    return Job(
-      id: documentId,
-      name: name,
-      ratePerHour: ratePerHour
-    );
+    return Job(id: documentId, name: name, ratePerHour: ratePerHour);
   }
   
   Map<String, dynamic> toMap() {
@@ -24,5 +21,18 @@ class Job {
       'name': name,
       'ratePerHour': ratePerHour,
     };
+  }
+
+  @override
+  int get hashCode => hashValues(id, name, ratePerHour);
+
+  @override
+  bool operator ==(other) {
+    if (identical(this, other)) return true;
+    if (runtimeType != other.runtimeType) return false;
+    final Job otherJob = other;
+    return id == otherJob.id &&
+        name == otherJob.name &&
+        ratePerHour == otherJob.ratePerHour;
   }
 }
