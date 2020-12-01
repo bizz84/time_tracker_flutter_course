Hesamedin/TimeTracker

Add pumpEmailSignInForm method

test/email_sign_in_form_stateful_test.dart
@@ -1,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form_stateful.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class MockAuth extends Mock implements AuthBase {}
+
+void main() {
+
+  MockAuth mockAuth;
+
+  setUp(() {
+    mockAuth = MockAuth();
+  });
+
+  Future<void> pumpEmailSignInForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      Provider<AuthBase>(
+        builder: (_) => mockAuth,
+        child: MaterialApp(
+          home: Scaffold(body: EmailSignInFormStateful()),
+        ),
+      ),
+    );
+  }
+}

bizz84/time_tracker_flutter_course

test/email_sign_in_form_stateful_test.dart
@@ -1,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form_stateful.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class MockAuth extends Mock implements AuthBase {}
+
+void main() {
+
+  MockAuth mockAuth;
+
+  setUp(() {
+    mockAuth = MockAuth();
+  });
+
+  Future<void> pumpEmailSignInForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      Provider<AuthBase>(
+        create: (_) => mockAuth,
+        child: MaterialApp(
+          home: Scaffold(body: EmailSignInFormStateful()),
+        ),
+      ),
+    );
+  }
+}