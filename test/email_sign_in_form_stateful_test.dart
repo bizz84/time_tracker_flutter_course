691967fda59ee49a46dff882d0f18ec220dd5fef
Andrea Bizzotto <bizz84@gmail.com>
2020-12-01 11:14:52 +0000

    Stubbing mockAuth methods

--- test/email_sign_in_form_stateful_test.dart
+++ test/email_sign_in_form_stateful_test.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
@@ -6,6 +8,7 @@ import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form_state
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class MockAuth extends Mock implements AuthBase {}
+class MockUser extends Mock implements User {}
 
 void main() {
   MockAuth mockAuth;
@@ -27,6 +30,16 @@ void main() {
         ),
       ),
     );
+  }
+
+  void stubSignInWithEmailAndPasswordSucceeds() {
+    when(mockAuth.signInWithEmailAndPassword(any, any))
+        .thenAnswer((_) => Future<User>.value(MockUser()));
+  }
+
+  void stubSignInWithEmailAndPasswordThrows() {
+    when(mockAuth.signInWithEmailAndPassword(any, any))
+        .thenThrow(FirebaseAuthException(code: 'ERROR_WRONG_PASSWORD'));
   }
 
   group('sign in', () {
@@ -53,6 +66,8 @@ void main() {
       var signedIn = false;
       await pumpEmailSignInForm(tester, onSignedIn: () => signedIn = true);
 
+      stubSignInWithEmailAndPasswordSucceeds();
+
       const email = 'email@email.com';
       const password = 'password';
 
@@ -71,6 +86,36 @@ void main() {
 
       verify(mockAuth.signInWithEmailAndPassword(email, password)).called(1);
       expect(signedIn, true);
+    });
+
+    testWidgets(
+        'WHEN user enters an invalid email and password'
+        'AND user taps on the sign-in button'
+        'THEN signInWithEmailAndPassword is called'
+        'AND user is not signed in', (WidgetTester tester) async {
+      var signedIn = false;
+      await pumpEmailSignInForm(tester, onSignedIn: () => signedIn = true);
+
+      stubSignInWithEmailAndPasswordThrows();
+
+      const email = 'email@email.com';
+      const password = 'password';
+
+      final emailField = find.byKey(Key('email'));
+      expect(emailField, findsOneWidget);
+      await tester.enterText(emailField, email);
+
+      final passwordField = find.byKey(Key('password'));
+      expect(passwordField, findsOneWidget);
+      await tester.enterText(passwordField, password);
+
+      await tester.pump();
+
+      final signInButton = find.text('Sign in');
+      await tester.tap(signInButton);
+
+      verify(mockAuth.signInWithEmailAndPassword(email, password)).called(1);
+      expect(signedIn, false);
     });
   });
 
