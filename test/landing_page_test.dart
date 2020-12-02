e87679f419063f0028f5f0e3508465e611945da9
Andrea Bizzotto <bizz84@gmail.com>
2020-12-02 09:30:53 +0000

    Adding tests to the landing page

--- test/landing_page_test.dart
+++ test/landing_page_test.dart
@@ -1,19 +1,36 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/home_page.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class MockAuth extends Mock implements AuthBase {}
-class MockUser extends Mock implements User {}
+class MockUser extends Mock implements User {
+  MockUser();
+  factory MockUser.uid(String uid) {
+    final user = MockUser();
+    when(user.uid).thenReturn(uid);
+    return user;
+  }
+}
 
 void main() {
   MockAuth mockAuth;
+  StreamController<User> onAuthStateChangedController;
 
   setUp(() {
     mockAuth = MockAuth();
+    onAuthStateChangedController = StreamController<User>();
+  });
+
+  tearDown(() {
+    onAuthStateChangedController.close();
   });
 
   Future<void> pumpLandingPage(WidgetTester tester) async {
@@ -25,11 +42,39 @@ void main() {
         ),
       ),
     );
+    await tester.pump();
   }
 
   void stubOnAuthStateChangedYields(Iterable<User> onAuthStateChanged) {
+    onAuthStateChangedController.addStream(
+      Stream<User>.fromIterable(onAuthStateChanged),
+    );
     when(mockAuth.authStateChanges()).thenAnswer((_) {
-      return Stream<User>.fromIterable(onAuthStateChanged);
+      return onAuthStateChangedController.stream;
     });
   }
+
+  testWidgets('stream waiting', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('null user', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([null]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(SignInPage), findsOneWidget);
+  });
+
+  testWidgets('non-null user', (WidgetTester tester) async {
+    stubOnAuthStateChangedYields([MockUser.uid('123')]);
+
+    await pumpLandingPage(tester);
+
+    expect(find.byType(HomePage), findsOneWidget);
+  });
 }
